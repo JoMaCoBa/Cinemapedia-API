41454405357520a921bbe614db477ef9928d93ed
JoMaCoBa <correabajo@gmail.com>
2024-07-12 08:25:51 -0600

    Added: Added to call to create a tables in the models

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
 from app.routers.movies import movies_router
+from app.models.movie import Movie
+from app.config.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Cinemapedia', version='0.0.1')
 
